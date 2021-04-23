784f40f57e3ca19886c7d06f5a2e20e6ba423d49
宇佐美隆一 <80155159+chateight@users.noreply.github.com>
2021-04-23 15:09:59 +0900

    Add files via upload
    little modification added; only document style

--- weathercast.py
+++ weathercast.py
@@ -10,11 +10,12 @@ req.urlretrieve(url, filename)
 # ダウンロードしたファイルを開く
 with open('tenki.json', 'r', encoding="UTF-8") as f:
     data = json.load(f)
-# 読み出したデータを解析 --- (*2) 
+# 読み出したデータを解析 
 for area in data:
     name = area['name']
     print("[", name, "]")
     for ts in area['srf']['timeSeries']:
         times = [n for n in ts['timeDefines']] 
         if 'weathers' in ts['areas']:
-            for i,v in enumerate(ts['areas']['weathers']): print(times[i], ":", v)
+            for i,v in enumerate(ts['areas']['weathers']): 
+                print(times[i], ":", v)
